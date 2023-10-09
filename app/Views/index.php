62078aeb9b0130844069dcb735caf31c375c0433
danuri <danualbantani@gmail.com>
2023-10-09 14:23:15 +0700

    Update index.php

--- app/Views/index.php
+++ app/Views/index.php
@@ -20,7 +20,23 @@
                 </div>
             </div>
         </div>
-        <!-- end page title -->
+
+        <div class="row">
+          <div class="col-xl-6">
+            <div class="card">
+              <div class="card-body">
+                <h4>Informasi Verifikasi</h4>
+                <ul>
+                  <li>Login ke <a href="https://verifikasi-sscasn.bkn.go.id" target="_blank">https://verifikasi-sscasn.bkn.go.id</a></li>
+                  <li>Verifikator: NIP, Password: @Vrf2023</li>
+                  <li>Supervisor: NIP, Password: @Spv2023</li>
+                  <li>Data Pelamar bisa diunduh melalui menu <a href="https://ropeg.kemenag.go.id/admincasn/downloads">Download Data</a></li>
+                  <li>Proses Verifikasi Seleksi Administrasi s.d Tanggal 12 Oktober 2023 Pukul 23:59 WIB</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
 
     </div>
     <!-- container-fluid -->
