c67ed50f4e3752c7402ea8e4855ba58e1f9b031f
danuri <danualbantani@gmail.com>
2023-11-24 13:32:49 +0700

    Update template.php

--- app/Views/template.php
+++ app/Views/template.php
@@ -96,14 +96,6 @@
                     <div class="dropdown-menu dropdown-menu-end">
                         <!-- item-->
                         <h6 class="dropdown-header"><?= session('nama')?><</h6>
-                        <a class="dropdown-item" href="pages-profile.html"><i class="mdi mdi-account-circle text-muted fs-16 align-middle me-1"></i> <span class="align-middle">Profile</span></a>
-                        <a class="dropdown-item" href="apps-chat.html"><i class="mdi mdi-message-text-outline text-muted fs-16 align-middle me-1"></i> <span class="align-middle">Messages</span></a>
-                        <a class="dropdown-item" href="apps-tasks-kanban.html"><i class="mdi mdi-calendar-check-outline text-muted fs-16 align-middle me-1"></i> <span class="align-middle">Taskboard</span></a>
-                        <a class="dropdown-item" href="pages-faqs.html"><i class="mdi mdi-lifebuoy text-muted fs-16 align-middle me-1"></i> <span class="align-middle">Help</span></a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="pages-profile.html"><i class="mdi mdi-wallet text-muted fs-16 align-middle me-1"></i> <span class="align-middle">Balance : <b>$5971.67</b></span></a>
-                        <a class="dropdown-item" href="pages-profile-settings.html"><span class="badge bg-soft-success text-success mt-1 float-end">New</span><i class="mdi mdi-cog-outline text-muted fs-16 align-middle me-1"></i> <span class="align-middle">Settings</span></a>
-                        <a class="dropdown-item" href="auth-lockscreen-basic.html"><i class="mdi mdi-lock text-muted fs-16 align-middle me-1"></i> <span class="align-middle">Lock screen</span></a>
                         <a class="dropdown-item" href="<?= site_url('auth/logout')?>"><i class="mdi mdi-logout text-muted fs-16 align-middle me-1"></i> <span class="align-middle" data-key="t-logout">Logout</span></a>
                     </div>
                 </div>
@@ -213,9 +205,11 @@
                             </a>
                             <div class="menu-dropdown collapse" id="sidebarLanding" style="">
                                 <ul class="nav nav-sm flex-column">
+                                  <?php if(session('is_kanwil') == 1){ ?>
                                     <li class="nav-item">
                                         <a href="<?= site_url('sktt/lokasi')?>" class="nav-link" data-key="t-one-page">Lokasi Ujian</a>
                                     </li>
+                                  <?php } ?>
                                     <!-- <li class="nav-item">
                                         <a href="nft-landing.html" class="nav-link" data-key="t-nft-landing">Peserta Ujian</a>
                                     </li>
